7cccb3579b87de1df8a9f2a02e5fe83de88dc309
LX <93909675+0Lx0@users.noreply.github.com>
2022-12-11 11:47:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/Stylish-Text-Bot.git /Stylish-Text-Bot
-cd /Stylish-Text-Bot
+git clone https://github.com/0Lx0/LxFontBot.git /LxFontBot
+cd /LxFontBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
